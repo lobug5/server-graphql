420d0db0fcad3e8ab1270aa22a28acf9f1bb342c
Lorena Santana <lo.santana96@gmail.com>
2019-10-23 21:34:12 -0300

    Subindo os arquivos do server

---
+++ data.js
@@ -0,0 +1,36 @@
+const data = {
+  channels: [
+    {
+      idChannel: 1,
+      name: 'Lobug DevGirl'
+    },
+    {
+      idChannel: 2,
+      name: 'Porta dos Fundos'
+    }
+  ],
+  playlists: [
+    {
+      idPlaylist: 1,
+      idChannel: 1,
+      name: 'GraphQL'
+    },
+    {
+      idPlaylist: 2,
+      idChannel: 1,
+      name: 'Node.js'
+    }
+  ],
+  videos: [
+    {
+      idVideo: 1,
+      idPlaylist: 1,
+      title: "Criando uma API com GraphQL"
+    }
+  ]
+
+};
+
+exports.getData = function(collection, field, value){
+  return data[collection].filter(element => element[field] === value);
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+
+const {GraphQLServer} = require('graphql-yoga');
+const data = require('./data');
+
+const typeDefs = `
+  type Channel{
+    idChannel: Int,
+    name: String,
+    playlists: [Playlist],
+  }
+
+  type Playlist{
+    idPlaylist: Int,
+    name: String
+    videos: [Video]
+  }
+
+  type Video{
+    idVideo: Int,
+    title: String
+  }
+
+  type Query{
+    channels: [Channel],
+  
+    channel(idChannel: Int): [Channel]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    // Retorna todas as informações sem o where
+    channels(){
+      return data.getData('channels');
+    },
+
+    channel(obj, args){
+      return data.getData('channels', 'idChannel', args.idChannel);
+    }
+
+  },
+  Channel: {
+    playlists: function(obj, args){
+      return data.getData('playlists', 'idChannel', obj.idChannel);
+    }
+  },
+  Playlist:{
+    videos: function(obj, args){
+      return data.getData('videos', 'idPlaylist', obj.idPlaylist);
+    }
+  }
+};
+
+const server = new GraphQLServer({typeDefs, resolvers})
+server.start();
